4d63b1ab5644da2a71dee1cf7cbe723fe2b8a042
Brunno Moreira <brunnomoreira.silva@gmail.com>
2015-11-11 08:00:33 -0200

    hotfix package name

--- src/br/com/pronatal/DAO/MarkerDAO.java
+++ src/br/com/pronatal/DAO/MarkerDAO.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package br.com.pronatal.DAO;
+package br.com.pronatal.dao;
 
 import java.io.Serializable;
 import java.util.ArrayList;
