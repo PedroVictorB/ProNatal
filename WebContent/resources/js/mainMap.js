4685a5b10ba0378b5b8fdb9fcbbe323414442fa1
Brunno Moreira <brunnomoreira.silva@gmail.com>
2015-12-08 18:14:56 -0200

    melhorando o carregamento de problemas no mapa

--- WebContent/resources/js/mainMap.js
+++ WebContent/resources/js/mainMap.js
@@ -20,56 +20,6 @@ function createMarker(latlng, html) {
         infowindow.setContent(contentString);
         infowindow.open(map, marker);
     });
-
-    google.maps.event.addListener(infowindow, 'domready', function () {
-
-        // Referência ao DIV que agrupa o fundo da infowindow
-        var iwOuter = $('.gm-style-iw');
-
-        /* Uma vez que o div pretendido está numa posição anterior ao div .gm-style-iw.
-         * Recorremos ao jQuery e criamos uma variável iwBackground,
-         * e aproveitamos a referência já existente do .gm-style-iw para obter o div anterior com .prev().
-         */
-        var iwBackground = iwOuter.prev();
-
-        // Remover o div da sombra do fundo
-        iwBackground.children(':nth-child(2)').css({'display': 'none'});
-
-        // Remover o div de fundo branco
-        iwBackground.children(':nth-child(4)').css({'display': 'none'});
-
-        // Desloca a infowindow 115px para a direita
-        iwOuter.parent().parent().css({left: '115px'});
-
-        // Desloca a sombra da seta a 76px da margem esquerda 
-        iwBackground.children(':nth-child(1)').attr('style', function (i, s) {
-            return s + 'left: 76px !important;'
-        });
-
-        // Desloca a seta a 76px da margem esquerda 
-        iwBackground.children(':nth-child(3)').attr('style', function (i, s) {
-            return s + 'left: 76px !important;'
-        });
-
-        // Altera a cor desejada para a sombra da cauda
-        iwBackground.children(':nth-child(3)').find('div').children().css({'box-shadow': 'rgba(72, 181, 233, 0.6) 0px 1px 6px', 'z-index': '1'});
-
-        // Referência ao DIV que agrupa os elementos do botão fechar
-        var iwCloseBtn = iwOuter.next();
-
-        // Aplica o efeito desejado ao botão fechar
-        iwCloseBtn.css({opacity: '1', right: '38px', top: '3px', border: '7px solid #48b5e9', 'border-radius': '13px', 'box-shadow': '0 0 5px #3990B9'});
-
-        // Se o conteúdo da infowindow não ultrapassar a altura máxima definida, então o gradiente é removido.
-        if ($('.iw-content').height() < 140) {
-            $('.iw-bottom-gradient').css({display: 'none'});
-        }
-
-        // A API aplica automaticamente 0.7 de opacidade ao botão após o evento mouseout. Esta função reverte esse evento para o valor desejado.
-        iwCloseBtn.mouseout(function () {
-            $(this).css({opacity: '1'});
-        });
-    });
 }
 
 initMap();
